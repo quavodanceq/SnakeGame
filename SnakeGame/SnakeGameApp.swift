fe36150c909b46438c70d1e8e18d9d200ef09006
quavodanceq <kuat.oralbekov1011@mail.ru>
2023-08-23 17:01:01 +0600

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Куат Оралбеков on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
